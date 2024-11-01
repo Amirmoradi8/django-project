6ae034f5b965ea0614147229fd7d796d2e475323
AmirMohammad <155167947+Amirmoradi8@users.noreply.github.com>
2024-11-02 00:40:01 +0330

    django-project

--- services/urls.py
+++ services/urls.py
@@ -6,6 +6,7 @@ app_name = 'services'
 
 urlpatterns = [
     path('' , services , name='services'),
+    # path('<str:category>' , name='list_by_category'),
     path('service-details' , services_details , name='services-details'),
     path('quote/' , quote , name='quote'),
 ]
--- services/views.py
+++ services/views.py
@@ -5,10 +5,33 @@ from .models import Specialservice
 # Create your views here.
 
 def services(request):
-    context = {
-        'services' : Service.objects.filter(status=True),
+# def services(request , category=None):
+#     if category:
+        # all_service = Service.objects.filter(category__title=category)
+    if request.GET.get('category') is not None:
+        # all_service = Service.objects.filter(category__title=request.GET.get('category'))
+        context = {
+        'services' : Service.objects.filter(category__title=request.GET.get('category')),
         'specials' : Specialservice.objects.filter(status=True),
     }
+    elif request.GET.get('search') is not None:
+        # all_service = Service.objects.filter(content__contains=request.GET.get('search'))
+        context = {
+        'services' : Service.objects.filter(content__contains=request.GET.get('search')),
+        'specials' : Specialservice.objects.filter(status=True),
+    }
+    elif request.GET.get('price') is not None:
+        # all_service = Service.objects.filter(price__lte=request.GET.get('price'))
+        context = {
+            'services' : Service.objects.filter(price__lte=request.GET.get('price')),
+            'specials' : Specialservice.objects.filter(status=True)
+        }
+    else:
+        # all_service = Specialservice.objects.filter(status=True)
+        context = {
+        'services' : Service.objects.filter(status=True),
+        'specials' : Specialservice.objects.filter(status=True)
+        }
     return render(request , 'services/services.html' , context = context)
 
 def services_details(request):
